19b7e43690d875ee5e9326cc21acd9ec21bd56de
TamilselvanChandrakumar <tamilbin001@gmail.com>
2024-02-06 11:20:32 +0530

    first commit

---
+++ business-website/src/components/home/Home.js
@@ -0,0 +1,48 @@
+import React from "react";
+import BannerImage from "../../assets/banner/banner-img.jpg";
+const Home = () => {
+  return (
+    <>
+      <section id="home">
+        <div className="banner_image"></div>
+        <div className="container">
+          <div className="banner_outer">
+            <div className="col">
+              <h3 className="title">
+                WE PROMOTE YOUR <span>BUSINESS</span>
+              </h3>
+              <p>
+                Nemo enim ipsam oluptatem quia reoluptas sit aspernatur aut odit
+                aut fugit, sed quia consequuntur magni dolores eosqui ratione.
+              </p>
+              <div className="btn_wrapper">
+                <a className="btn" href="/">
+                  Get Started
+                </a>
+              </div>
+            </div>
+            <div className="col">
+              <div className="sub_banner_image">
+                <img src={BannerImage} alt="bannerimg"></img>
+              </div>
+              <div className="banner_style_1">
+                <></>
+                <h4>Business Analytic</h4>
+              </div>
+              <div className="banner_style_1 banner_style_2">
+                <></>
+                <h4>99.9% Sucess</h4>
+              </div>
+              <div className="banner_style_1 banner_style_3">
+                <></>
+                <h4>Strategy</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Home;
