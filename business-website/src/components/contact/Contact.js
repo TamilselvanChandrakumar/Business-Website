52efdd002569d8ef7dda966777a9c7379eef4b1d
TamilselvanChandrakumar <tamilbin001@gmail.com>
2024-03-02 23:40:33 +0530

    first commit

--- business-website/src/components/contact/Contact.js
+++ business-website/src/components/contact/Contact.js
@@ -3,7 +3,47 @@ import "../../components/contact/Contact.css";
 const Contact = () => {
   return (
     <section id="contact">
-      <div className="container"></div>
+      <div className="container">
+        <div className="contact_wrapper">
+          <div className="contact_col">
+            <div className="contact_image">
+              <img alt=""></img>
+            </div>
+          </div>
+          <div className="contact_col">
+            <h2>Contact Us</h2>
+            <form>
+              <div className="input_wrapper">
+                <input
+                  type="text"
+                  className="form_controll"
+                  placeholder="your name..."
+                  autoComplete="off"
+                ></input>
+              </div>
+              <div className="input_wrapper">
+                <input
+                  type="text"
+                  className="form_controll"
+                  placeholder="your phone..."
+                  autoComplete="off"
+                ></input>
+              </div>
+              <div className="input_wrapper">
+                <input
+                  type="email"
+                  className="form_controll"
+                  placeholder="your email..."
+                  autoComplete="off"
+                ></input>
+              </div>
+              <div className="input_wrapper">
+                <textarea placeholder="write a message..."></textarea>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
